ce5f0766871e86703f14c069c827d536ce881aa2
Gaston Roxas <gacaro51597@gmail.com>
2023-03-17 14:21:41 +0800

    fix locations list ui

--- src/components/locations/LocationsList.tsx
+++ src/components/locations/LocationsList.tsx
@@ -20,15 +20,23 @@ const LocationsList = () => {
 
   return (
     <div className="bg-white container mx-auto">
-      <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex justify-between">
+      <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
         <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
           Locations
         </h1>
 
-        <button className="" onClick={() => setCreateLocationModal(true)}>
+        <button
+          className="h-8 bg-green-600 hover:bg-green-700 text-white px-4 rounded"
+          onClick={() => setCreateLocationModal(true)}
+        >
           Create new location
         </button>
       </div>
+
+      <div className="relative flex py-5 items-center">
+        <div className="flex-grow border-t border-gray-400"></div>
+      </div>
+
       <div>
         <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
           <div className="grid gap-5 max-w-lg mx-auto lg:grid-cols-3 lg:max-w-none">
