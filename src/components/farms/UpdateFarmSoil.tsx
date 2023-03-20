8cf74861b4f22e53b1c30acd4cef51ace97e2f4f
Gaston Roxas <gacaro51597@gmail.com>
2023-03-20 16:48:24 +0800

    fix soil record form

--- src/components/farms/UpdateFarmSoil.tsx
+++ src/components/farms/UpdateFarmSoil.tsx
@@ -171,7 +171,7 @@ const UpdateFarmSoil = (props: UpdateFarmSoilProps) => {
                   type="submit"
                   className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
                 >
-                  Add weather
+                  Add soil record
                 </button>
               </div>
             </form>
